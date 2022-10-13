21890099c4fa318e46030627b41b2675ae6fa9c1
Moreart1 <Morearti1726263@yandex.ru>
2022-10-13 14:07:33 +0300

    Урок 1. Принципы асинхронных и синхронных взаимодействий

---
+++ Restaurant/Restaurant/Table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Table
+    {
+        public TableState State { get; private set; }
+        public int SeatsCount { get; }
+        public int Id { get; }
+
+        public Table(int id)
+        {
+            Id = id;
+            State = TableState.Free;
+            SeatsCount = Random.Shared.Next(2, 5);
+        }
+
+        
+        public bool Set(TableState state)
+        {
+            if (state == State) return false;
+
+            State = state;
+            return true;
+        }
+    }
+}
